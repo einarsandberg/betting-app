4e69c3e391a4a0f0d9a10337ae323cdf9671c097
Einar Sandberg <einars92@gmail.com>
2020-02-27 14:28:03 +0100

    added login component

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './Login.css';
+import { useState } from 'react';
 
-const Login: React.FC = () => {
+type LoginProps = {
+    loginCallback: (userName: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = (props: LoginProps) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="Login">
             <div className="Login-background"></div>
             <div className="Login-form">
-                <input type="text" placeholder="Email" className="Login-form__field"></input>
-                <input type="password" placeholder="Password" className="Login-form__field"></input>
-                <div className="Login-form__submit">Login</div>
+                <input 
+                    className="Login-form__field" 
+                    type="text" 
+                    placeholder="Email"
+                    onChange={(e): void => setEmail(e.target.value)}
+                ></input>
+                <input 
+                    className="Login-form__field" 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={(e): void => setPassword(e.target.value)}
+                ></input>
+                <div 
+                    className="Login-form__submit"
+                    onClick={(): void => props.loginCallback(email, password)}
+                >
+                    Login
+                </div>
             </div>
         </div>
     );
