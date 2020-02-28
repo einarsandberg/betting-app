3dffe46c9c06ac0cefaa2522b1bd828fb02a3640
Einar Sandberg <einars92@gmail.com>
2020-02-28 15:07:58 +0100

    get matches

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { User } from '../api/AuthService';
+import { useEffect } from 'react';
+import MatchService, { Match } from '../api/MatchService';
+import Bet from './Bet';
+
+const matchService = new MatchService();
+
 type HomeProps = {
     user: User;
 }
+
 const Home: React.FC<HomeProps> = (props: HomeProps) => {
+    const [matches, setMatches] = useState<Match[]>([]);
+    
+    useEffect(() => {
+        async function getMatches(): Promise<void> {
+            try {
+                const matches = await matchService.getMatches();
+                setMatches(matches);
+            } catch(err) {
+                //
+            }
+        }
+        getMatches();
+    }, []);
 
     return (
         <div className="Home">
-            Welcome {props.user.firstName}
+            <Bet matches={matches} />
         </div>
     );
 };
