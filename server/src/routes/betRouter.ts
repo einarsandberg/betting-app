81b5b2cb346024922a7381f4b11deb5ae573670b
Einar Sandberg <einars92@gmail.com>
2020-03-02 16:01:58 +0100

    added get current bet endpoint

--- server/src/routes/betRouter.ts
+++ server/src/routes/betRouter.ts
@@ -22,4 +22,22 @@ router.post('/', async (req: ApiRequest<MatchBet[]>, res) => {
     } 
 });
 
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: res.locals.email });
+        if (!user) return res.status(500).send('User not found');
+
+        const bet = await Bet.findOne({ userId: user._id });
+
+        if (!bet) return res.json([]);
+        
+        res.json(bet.matches);
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('An error occured');
+    } 
+});
+
+
 export default router;
