4062477bbda3270473c67720164cbd11fdbcf086
Einar Sandberg <einars92@gmail.com>
2020-03-02 12:50:23 +0100

    added bet submit button

--- client/src/components/Bet/Bet.tsx
+++ client/src/components/Bet/Bet.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Match, MatchResult } from '../../api/MatchService';
 import Round from './Round';
 import './Bet.css';
+
 type BetProps = {
     matches: Match[];
-    setBet: React.Dispatch<React.SetStateAction<MatchResult[]>>;
 };
 
 const Bet: React.FC<BetProps> = (props: BetProps) => {
+    const [bet, setBet] = useState<MatchResult[]>([]);
     
     const rounds = props.matches.reduce((acc: Match[][], curr: Match) => {
         acc[curr.round] = acc[curr.round] || [];
@@ -15,10 +16,11 @@ const Bet: React.FC<BetProps> = (props: BetProps) => {
         return acc;
     }, []);
 
-    const roundEls = rounds.map((matches: Match[], i) => <Round key={'round' + i} matches={matches} setBet={props.setBet}/>);
+    const roundEls = rounds.map((matches: Match[], i) => <Round key={'round' + i} matches={matches} setBet={setBet}/>);
     return (
         <div className="Bet">
             { roundEls }
+            <div className="Bet__submit">Submit</div>
         </div>
     );
 };
